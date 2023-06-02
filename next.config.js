af1f76b1f17b70a791dbf0e60746159afa6190bd
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2023-06-02 12:57:25 +0200

    test publish action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = withNextra()
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = Object.assign(
+  {
+    assetPrefix: isProd ? "/colevandersWands.github.io/" : "",
+    images: {
+      unoptimized: true,
+    },
+  },
+  withNextra()
+);
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Evan Cole</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/colevandersWands",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase:
+    "https://github.com/colevandersWands/colevandersWands.github.io",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Left Foot",
   },
-}
+};
 
-export default config
+export default config;
